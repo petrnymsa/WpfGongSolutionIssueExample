c792aa28b76b2c335c0ca7488743c71c451b047c
Petr Nymsa <nymsa.petr@outlook.cz>
2018-12-05 10:47:12 +0100

    example

---
+++ WpfGongSolutions/SimpleTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfGongSolutions
+{
+    class SimpleTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = container as FrameworkElement;
+
+            if (element != null && item != null && item is SampleModel)
+            {
+                var sampleModel = item as SampleModel;
+
+                if (sampleModel.IsSmall)
+                    return
+                        element.FindResource("SmallTileTemplate") as DataTemplate;
+                else
+                    return
+                        element.FindResource("MediumTileTemplate") as DataTemplate;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ WpfGongSolutions/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfGongSolutions
+{
+    public class SampleModel
+    {
+        public string Name { get; set; }
+        public bool IsSmall { get; set; }
+
+        public SampleModel(string name, bool isSmall = false)
+        {
+            Name = name;
+            IsSmall = isSmall;
+        }
+    }
+
+    public class ViewModel
+    {
+        public ObservableCollection<SampleModel> Data { get; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<SampleModel>();
+            for(var i = 1; i < 7; i++) 
+                Data.Add(new SampleModel($"Item-{i}", i % 2 != 0));
+        }
+    }
+}
